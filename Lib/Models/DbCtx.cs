b1e30ed8e787cb0db6b86105aa7a1b6545c31a9d
sn <86227911+tbui17@users.noreply.github.com>
2024-06-25 14:57:37 -0600

    Initial migration

--- Lib/Models/DbCtx.cs
+++ Lib/Models/DbCtx.cs
@@ -1,6 +1,15 @@
-﻿namespace Lib.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class DbCtx
+namespace Lib.Models;
+
+public class DbCtx : DbContext
 {
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder b)
+    {
+        b.UseSqlite
+        (
+            "Data Source=db.sqlite",
+            x => x.MigrationsAssembly("Data")
+        );
+    }
 }
