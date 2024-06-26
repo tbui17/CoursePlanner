23981366696d6c25f5d3b19a15faebc6b21b5c2e
sn <86227911+tbui17@users.noreply.github.com>
2024-06-26 15:00:40 -0600

    Basic frontend-backend integration

--- CoursePlanner/MainPage.xaml.cs
+++ CoursePlanner/MainPage.xaml.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using Microsoft.Maui.Accessibility;
-using Microsoft.Maui.Controls;
+﻿using ViewModels;
 
-namespace CoursePlanner
+namespace CoursePlanner;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+    public MainViewModel Model { get; set; }
+
+    public MainPage(MainViewModel model)
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        Model = model;
+        InitializeComponent();
+        BindingContext = this;
     }
 
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
