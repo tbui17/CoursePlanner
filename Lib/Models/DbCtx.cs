d4c5202d0f7d38826c24d5751df4e1af0aabf0ae
sn <86227911+tbui17@users.noreply.github.com>
2024-06-25 18:18:34 -0600

    Modify assessment hierarchy

--- Lib/Models/DbCtx.cs
+++ Lib/Models/DbCtx.cs
@@ -7,7 +7,8 @@ public class DbCtx : DbContext
     public DbSet<User> User { get; set; }
     public DbSet<Term> Term { get; set; }
     public DbSet<TermCourse> TermCourse { get; set; }
-    public DbSet<AssessmentCourse> AssessmentCourse { get; set; }
+    public DbSet<PerformanceAssessmentCourse> AssessmentCourse { get; set; }
+    public DbSet<ObjectiveAssessmentCourse> ObjectiveAssessmentCourse { get; set; }
     public DbSet<Course> Course { get; set; }
     public DbSet<Assessment> Assessment { get; set; }
     public DbSet<Note> Note { get; set; }
