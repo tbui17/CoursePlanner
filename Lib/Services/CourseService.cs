b31478f219f9c2f7504e4114f508c8e4534c65fc
* <tbui45@wgu.edu>
2024-08-03 12:43:23 -0600

    CourseService.cs Begin gradual detailed course vm refactor Changed course vm to deselect items upon refresh Update container configs

---
+++ Lib/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using Lib.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lib.Services;
+
+public interface ICourseService
+{
+    Task<Course?> GetFullCourse(int id);
+}
+
+public class CourseService(IDbContextFactory<LocalDbCtx> factory) : ICourseService
+{
+
+
+    public async Task<Course?> GetFullCourse(int id)
+    {
+        await using var db = await factory.CreateDbContextAsync();
+        var course = await db
+           .Courses
+           .Include(x => x.Instructor)
+           .Include(x => x.Assessments)
+           .Include(x => x.Notes)
+           .FirstOrDefaultAsync(x => x.Id == id);
+
+        return course;
+    }
+
+}
