3208504866ff070d3505fc85f0f937695cfdfb6a
* <tbui45@wgu.edu>
2024-08-03 10:55:46 -0600

    Set up view model test foundation

---
+++ ViewModelTests/BasePageViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using Lib.Models;
+
+namespace ViewModelTests;
+
+public class BasePageViewModelTest<T> : BaseDbTest where T : notnull
+{
+    [SetUp]
+    public override async Task Setup()
+    {
+        await base.Setup();
+        Db = GetDbFactory()();
+        Model = Resolve<T>();
+    }
+
+    protected T Model { get; set; }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Db.Dispose();
+    }
+
+    protected LocalDbCtx Db { get; set; }
+
+}
