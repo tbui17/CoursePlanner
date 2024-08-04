ce2b5b7a10db64b65301900bb6109c65b6108eac
* <tbui45@wgu.edu>
2024-08-03 20:41:16 -0600

    Add local notification service tests

--- ViewModelTests/LocalNotificationServiceTests.cs
+++ ViewModelTests/LocalNotificationServiceTests.cs
@@ -1,28 +1,57 @@
-﻿using Plugin.LocalNotification;
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Lib.Models;
+using Plugin.LocalNotification;
 using ViewModels.Services;
 
 namespace ViewModelTests;
 
 public class LocalNotificationServiceTests
 {
-    [Test]
-    public async Task METHOD()
+    private Course _course;
+
+    [SetUp]
+    public async Task Setup()
     {
-        var localNotificationService = Resolve<ILocalNotificationService>();
-        await localNotificationService.Notify(new NotificationRequest
-            {
-                NotificationId = 2,
-                Title = "Abc",
-                Description = "Def"
-            }
-        );
+        // setup to have 1 upcoming course
 
         await using var db = GetDb();
+        var course = db.Courses.First();
+        course.Start = DateTime.Now.AddHours(5);
+        course.ShouldNotify = true;
+        await db.SaveChangesAsync();
+        _course = course;
 
+    }
 
+    [Test]
+    public async Task SendUpcomingNotifications_1UpcomingEvent_MessageShouldContainNotificationNameAndCount()
+    {
 
+        // subscribe to notifications
 
+        var service = Resolve<ILocalNotificationService>();
+        List<NotificationRequest> requests = [];
+        service.NotificationReceived += (_, args) => requests.Add(args.Request);
 
+        // check db for upcoming notifications and publish
 
+        await service.SendUpcomingNotifications();
+
+        using var _ = new AssertionScope();
+
+        var req = requests
+           .Should()
+           .ContainSingle()
+           .Subject;
+
+        req
+           .Title
+           .Should()
+           .Contain("1");
+        req
+           .Description
+           .Should()
+           .Contain(_course.Name);
     }
 }
