f364ff259bc47d06e33f1f0cf7fc81a476ca507e
* <tbui17@users.noreply.github.com>
2024-08-14 23:46:24 -0600

    modify to generic method

--- Lib/Traits/ValidationExtensions.cs
+++ Lib/Traits/ValidationExtensions.cs
@@ -6,21 +6,21 @@ namespace Lib.Traits;
 public static class ValidationExtensions
 {
 
-    public static DomainException? ValidateName(this IEntity entity)
+    public static DomainException? ValidateName<T>(this T entity) where T : IEntity
     {
         return string.IsNullOrWhiteSpace(entity.Name)
             ? new DomainException("Name cannot be null or empty")
             : null;
     }
 
-    public static DomainException? ValidateDates(this IDateTimeRange dateTimeRange)
+    public static DomainException? ValidateDates<T>(this T dateTimeRange) where T : IDateTimeRange
     {
         return dateTimeRange.Start >= dateTimeRange.End
             ? new DomainException("Start must be before end")
             : null;
     }
 
-    public static DomainException? ValidateNameAndDates(this IDateTimeEntity entity)
+    public static DomainException? ValidateNameAndDates<T>(this T entity) where T : IEntity, IDateTimeRange
     {
         return AggregateValidation(entity.ValidateName(), entity.ValidateDates());
     }
