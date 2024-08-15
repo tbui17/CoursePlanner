cdb9a8a58436a307e716e48e8075862192dbd57f
* <tbui17@users.noreply.github.com>
2024-08-15 11:52:53 -0600

    checkpoint1

---
+++ Lib/Traits/AssignmentTrait.cs
@@ -0,0 +1,23 @@
+﻿using Lib.Models;
+
+namespace Lib.Traits;
+
+public static class AssignmentTrait
+{
+    public static void Assign<T, T2>(this T form, T2 otherForm)
+        where T : IAssessmentForm where T2 : IAssessmentForm
+    {
+        form.Id = otherForm.Id;
+        form.Name = otherForm.Name;
+        form.Start = otherForm.Start;
+        form.End = otherForm.End;
+        form.ShouldNotify = otherForm.ShouldNotify;
+        form.Type = otherForm.Type;
+    }
+
+    public static void Assign<T>(this T form, Course course) where T : IAssessmentForm
+    {
+        var otherAssessment = Assessment.From(course);
+        form.Assign(otherAssessment);
+    }
+}
--- Lib/Traits/ValidationTrait.cs
+++ Lib/Traits/ValidationTrait.cs
@@ -3,7 +3,7 @@ using Lib.Interfaces;
 
 namespace Lib.Traits;
 
-public static class ValidationExtensions
+public static class ValidationTrait
 {
 
     public static DomainException? ValidateName<T>(this T entity) where T : IEntity
@@ -13,7 +13,7 @@ public static class ValidationExtensions
             : null;
     }
 
-    public static DomainException? ValidateDates<T>(this T dateTimeRange) where T : IDateTimeRange
+    private static DomainException? ValidateDates<T>(this T dateTimeRange) where T : IDateTimeRange
     {
         return dateTimeRange.Start >= dateTimeRange.End
             ? new DomainException("Start must be before end")
