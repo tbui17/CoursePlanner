a565c9d31b32ee9225703eecd82f88bc02daad26
* <tbui17@users.noreply.github.com>
2024-08-16 03:19:12 -0600

    impl v1 tests passing

--- ViewModels/PageViewModels/EditAssessmentViewModel.cs
+++ ViewModels/PageViewModels/EditAssessmentViewModel.cs
@@ -1,19 +1,275 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
 using CommunityToolkit.Maui.Core.Extensions;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Lib.Exceptions;
 using Lib.Interfaces;
 using Lib.Models;
 using Lib.Traits;
+using Lib.Utils;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Logging;
 using ViewModels.Services;
 
 namespace ViewModels.PageViewModels;
 
-public abstract partial class AssessmentFormViewModel(INavigationService navService)
-    : ObservableObject, IAssessmentAssociatedForm
+public partial class EditAssessmentViewModel(
+    ILocalDbCtxFactory factory,
+    INavigationService navService,
+    IAppService appService,
+    ILogger<EditAssessmentViewModel> logger
+)
+    : ObservableObject
 {
+    [ObservableProperty]
+    private int _id;
+
+    [ObservableProperty]
+    private ObservableCollection<AssessmentItemViewModel> _assessments = [];
+
+    [ObservableProperty]
+    private AssessmentItemViewModel? _selectedAssessment;
+
+    [ObservableProperty]
+    private Course _course = new();
+
+
+    public IEnumerable<Assessment> GetDbModels() =>
+        Assessments
+            .Select(x => new Assessment().Assign(x));
+
+
+    private static IEnumerable<DomainException> Validate(ICollection<Assessment> assessments)
+    {
+        var exceptions = new List<string>();
+        foreach (var assessment in assessments)
+        {
+            if (assessment.ValidateNameAndDates() is { } exc)
+            {
+                exceptions.Add($"Type: '{assessment.Type}', Name: '{assessment.Name}', Message: {exc.Message}");
+            }
+        }
+
+        if (ValidateUnique(assessments) is { } uniqueExc)
+        {
+            exceptions.Add(uniqueExc.Message);
+        }
+
+        return exceptions.Select(x => new DomainException(x));
+    }
+
+    private static DomainException? ValidateUnique(ICollection<Assessment> assessments)
+    {
+        return assessments.DistinctBy(x => x.Type).Count() != assessments.Count
+            ? new DomainException("Assessment types must be unique.")
+            : null;
+    }
+
+    public ObservableCollection<string> AssessmentTypes { get; } = Assessment.Types.ToObservableCollection();
+
+    [RelayCommand]
+    public async Task SaveAsync()
+    {
+        var assessmentCount = Assessments.Count;
+        logger.LogInformation("Assessment count: {AssessmentCount}", assessmentCount);
+        if (Assessments.Count == 0)
+        {
+            logger.LogInformation("No assessments to save.");
+            await BackAsync();
+            return;
+        }
+
+        var assessments = GetDbModels().ToImmutableList();
+
+        logger.LogInformation("Validating assessments.");
+
+        if (Validate(assessments).ToList() is { Count: > 0 } exceptions)
+        {
+            await ShowMessage();
+            return;
+        }
+
+        logger.LogInformation("Assessments are valid. Saving changes.");
+
+        await SaveChanges();
+        await BackAsync();
+        return;
+
+        async Task ShowMessage()
+        {
+            var message = GetMessage();
+
+            logger.LogInformation("Assessment validation failed: {Message}", message);
+            var appMessage = "The assessments failed to meet the following criteria: \n" + message;
+
+            await appService.ShowErrorAsync(appMessage);
+        }
+
+        string GetMessage()
+        {
+            return exceptions.Select(x => x.Message)
+                .StringJoin(Environment.NewLine);
+        }
+
+
+        async Task SaveChanges()
+        {
+            await using var db = await factory.CreateDbContextAsync();
+            await using var tx = await db.Database.BeginTransactionAsync(IsolationLevel.Serializable);
+
+            var ids = assessments.Select(x => x.Id).ToList();
+
+            logger.LogInformation("Updating assessments: {AssessmentIds}", ids);
+
+            var toUpdate = await db.Assessments
+                .AsTracking()
+                .Where(x => ids.Contains(x.Id))
+                .ToListAsync();
+
+            var pairs = from dbCopy in toUpdate
+                join local in assessments on dbCopy.Id equals local.Id
+                select (dbCopy, local);
+
+            foreach (var x in pairs)
+            {
+                x.dbCopy.Assign(x.local);
+            }
+
+
+            foreach (var model in assessments.Where(x => x.Id == 0))
+            {
+                db.Assessments.Add(model);
+            }
+
+            var changes = GetChanges(db.ChangeTracker);
+
+
+            logger.LogInformation("Changes: {}{Changes}", Environment.NewLine, changes);
+
+            await db.SaveChangesAsync();
+            await tx.CommitAsync();
+        }
+    }
+
+    private static string GetChanges(ChangeTracker tracker) =>
+        tracker
+            .Entries<Assessment>()
+            .GroupBy(x => x.State)
+            .Select(group => group.Aggregate(
+                new StringBuilder().AppendLine($"State: {group.Key}"),
+                (sb, entry) => sb.Append('\t').AppendLine($"Id: {entry.Entity.Id}, Name: {entry.Entity.Name}")))
+            .Select(x => x.ToString())
+            .StringJoin(Environment.NewLine);
+
+    [RelayCommand]
+    public async Task BackAsync()
+    {
+        await navService.PopAsync();
+    }
+
+
+    public async Task Init(int courseId)
+    {
+        Id = courseId;
+
+        await using var db = await factory.CreateDbContextAsync();
+
+        var course = await db
+            .Courses
+            .Where(x => x.Id == courseId)
+            .Include(x => x.Assessments)
+            .FirstAsync();
+
+        Course = course;
+
+        var assessments = course.Assessments;
+
+        Assessments = assessments
+            .Select(x => new AssessmentItemViewModel(x))
+            .ToObservableCollection();
+    }
+
+
+    [RelayCommand]
+    private async Task DeleteAssessmentAsync()
+    {
+        if (SelectedAssessment is null)
+        {
+            return;
+        }
+
+        var assessment = SelectedAssessment;
+        Assessments.Remove(assessment);
+        SelectedAssessment = null;
+
+        await using var db = await factory.CreateDbContextAsync();
+        await db
+            .Assessments
+            .Where(x => x.Id == assessment.Id)
+            .ExecuteDeleteAsync();
+    }
+
+    [RelayCommand]
+    public async Task AddAssessmentAsync()
+    {
+        logger.LogInformation("Adding Assessment. {AssessmentCount}", Assessments.Count);
+        if (Assessments.Count >= 2)
+        {
+            logger.LogInformation("{AssessmentCount} assessments already exist.", Assessments.Count);
+            await appService.ShowErrorAsync("Only 2 assessments allowed per course.");
+            return;
+        }
+
+        var assessment = CreateAssessment();
+        var vm = new AssessmentItemViewModel(assessment);
+        Assessments.Add(vm);
+        SelectedAssessment = vm;
+        return;
+
+        Assessment CreateAssessment()
+        {
+            var course = Course;
+            var newAssessment = course.CreateAssessment();
+
+            switch (Assessments.Count)
+            {
+                case >= 2:
+                    throw new UnreachableException("There should not be more than 2 assessments.");
+                case 0:
+                    return newAssessment;
+                default:
+                    var remainingAssessment = Assessments[0];
+
+                    newAssessment.EnsureOppositeType(remainingAssessment);
+
+                    return newAssessment;
+            }
+        }
+    }
+
+    public async Task RefreshAsync()
+    {
+        await Init(Id);
+    }
+}
+
+public partial class AssessmentItemViewModel
+    : ObservableObject, IAssessmentForm
+{
+    public AssessmentItemViewModel()
+    {
+    }
+
+    public AssessmentItemViewModel(Assessment assessment)
+    {
+        this.Assign(assessment);
+    }
+
     [ObservableProperty]
     private int _id;
 
@@ -29,165 +285,14 @@ public abstract partial class AssessmentFormViewModel(INavigationService navServ
     [ObservableProperty]
     private bool _shouldNotify;
 
-    public ObservableCollection<string> AssessmentTypes { get; set; } = Assessment.Types.ToObservableCollection();
-
     [ObservableProperty]
     private string _selectedAssessmentType = Assessment.Types.First();
 
-
-    string IAssessmentType.Type
+    public string Type
     {
         get => SelectedAssessmentType;
         set => SelectedAssessmentType = value;
     }
 
-
-    protected abstract Task SaveAsyncImpl();
-
-    [RelayCommand]
-    public async Task SaveAsync()
-    {
-        await SaveAsyncImpl();
-    }
-
-    [RelayCommand]
-    public async Task BackAsync()
-    {
-        await navService.PopAsync();
-    }
-
-    public abstract Task Init(int id);
-
-
-    public async Task RefreshAsync()
-    {
-        await Init(Id);
-    }
-}
-
-public class EditAssessmentViewModel(
-    ILocalDbCtxFactory factory,
-    INavigationService navService,
-    IAppService appService) : AssessmentFormViewModel(navService)
-{
-    protected override async Task SaveAsyncImpl()
-    {
-        if (this.ValidateNameAndDates() is { } exc)
-        {
-            await appService.ShowErrorAsync(exc.Message);
-            return;
-        }
-
-        await using var db = await factory.CreateDbContextAsync();
-        var assessment = await db
-           .Assessments
-           .AsTracking()
-           .FirstAsync(x => x.Id == Id);
-
-        assessment.Assign(this);
-
-        await db.SaveChangesAsync();
-        await BackAsync();
-    }
-
-    public override async Task Init(int assessmentId)
-    {
-        await using var db = await factory.CreateDbContextAsync();
-
-        var assessment = await db
-               .Assessments
-               .FirstOrDefaultAsync(x => x.Id == assessmentId) ??
-            new();
-
-        this.Assign(assessment);
-    }
-}
-
-public class AddAssessmentViewModel(
-    ILocalDbCtxFactory factory,
-    INavigationService navService,
-    IAppService appService,
-    ILogger<AddAssessmentViewModel> logger) : AssessmentFormViewModel(navService)
-{
-    private readonly INavigationService _navService = navService;
-
-    protected override async Task SaveAsyncImpl()
-    {
-        if (this.ValidateNameAndDates() is { } exc)
-        {
-            await appService.ShowErrorAsync(exc.Message);
-            return;
-        }
-
-        await using var db = await factory.CreateDbContextAsync();
-        var assessment = await db
-           .Assessments
-           .AsTracking()
-           .FirstAsync(x => x.Id == Id);
-
-        assessment.Assign(this);
-
-        await db.SaveChangesAsync();
-        await BackAsync();
-    }
-
-    public override async Task Init(int courseId)
-    {
-        await using var db = await factory.CreateDbContextAsync();
-        var course = await db
-           .Courses
-           .AsNoTracking()
-           .Include(x => x.Assessments)
-           .FirstAsync(x => x.Id == courseId);
-
-        if (AddChangeLog.Create(course) is not { } changelog)
-        {
-            logger.LogInformation("Changelog null. Returning. {CourseId}", course.Id);
-            await appService.ShowErrorAsync("Course already has 2 assessments. Returning to previous page.");
-            await _navService.GoBackToDetailedTermPageAsync();
-            return;
-        }
-
-        logger.LogInformation("Applying changelog to assessment. {Changelog}", changelog);
-
-        this.Assign(changelog);
-    }
-}
-
-public record AddChangeLog : IAssessmentAssociatedForm
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
-    public bool ShouldNotify { get; set; }
-    public string Type { get; set; } = Assessment.DefaultType;
-    public ObservableCollection<string> AssessmentTypes { get; set; } = [];
-
-    public static IAssessmentForm? Create(Course course)
-    {
-        if (course.Assessments.Count >= 2)
-        {
-            return null;
-        }
-
-        IAssessmentForm assessment = course.CreateAssessment();
-
-        if (course.Assessments.FirstOrDefault() is { } other)
-        {
-            assessment.EnsureOppositeType(other);
-            var noAssessmentChangelog = new AddChangeLog();
-            noAssessmentChangelog.Assign(assessment);
-            noAssessmentChangelog.AssessmentTypes = [];
-            return noAssessmentChangelog;
-        }
-
-        IAssessmentAssociatedForm oneAssessmentChangelog = new AddChangeLog();
-
-        oneAssessmentChangelog.Assign(course.CreateAssessment());
-
-        oneAssessmentChangelog.AssessmentTypes = [oneAssessmentChangelog.OppositeType];
-
-        return oneAssessmentChangelog;
-    }
+    public int CourseId { get; set; }
 }
