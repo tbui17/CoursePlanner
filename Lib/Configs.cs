ef4347a50b5ce26f3c8942dffa4bdf4314af2ad0
* <tbui17@users.noreply.github.com>
2024-08-17 14:54:40 -0600

    lib proj

---
+++ Lib/Configs.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Design;
+
+namespace Lib;
+
+public class Configs
+{
+
+    public static IServiceContainer ConfigBackendServices(IServiceContainer b)
+    {
+        return b;
+    }
+}
