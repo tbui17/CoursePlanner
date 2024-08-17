4bc9c79cd7967a4ac735d384c126e38478a6c8ba
* <tbui17@users.noreply.github.com>
2024-08-17 17:21:24 -0600

    [maui]platform code

---
+++ CoursePlanner/Platforms/Android/MainApplication.cs
@@ -0,0 +1,21 @@
+﻿using Android;
+using Android.App;
+using Android.Runtime;
+
+[assembly: UsesPermission(Manifest.Permission.WakeLock)]
+[assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
+[assembly: UsesPermission(Manifest.Permission.Vibrate)]
+[assembly: UsesPermission(Manifest.Permission.PostNotifications)]
+namespace CoursePlanner
+{
+
+    
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership) { }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
