ab5ea1d673089fb0ec75115d9cfb6f3de7fb6867
* <tbui17@users.noreply.github.com>
2024-08-17 17:22:27 -0600

    [lib]db setup util

---
+++ CoursePlanner/Utils/DbSetup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CoursePlanner.Utils;
+
+public class DbSetup(ILocalDbCtxFactory factory, ILogger<DbSetup> logger)
+{
+    public void SetupDb()
+    {
+        using var db = factory.CreateDbContext();
+        logger.LogInformation("Attempting to migrate database.");
+        try
+        {
+            db.Database.Migrate();
+            logger.LogInformation("Database migrated.");
+        }
+        catch (SqliteException e) when (e.Message.Contains("already exists"))
+        {
+            logger.LogInformation(
+                $"A database entity already exists. Attempting to re-initialize database. {e.Message}"
+            );
+            db.Database.EnsureDeleted();
+            db.Database.Migrate();
+            logger.LogInformation("Database re-initialized.");
+        }
+    }
+}
