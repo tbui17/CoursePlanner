9070e1721e9cc22ce05914b843f6cb731f0bedff
* <tbui17@users.noreply.github.com>
2024-08-17 17:21:11 -0600

    [maui]converters

---
+++ CoursePlanner/Converters/CourseListToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Lib.Models;
+
+namespace CoursePlanner.Converters;
+
+public class CourseListToStringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not ICollection<Course> courses)
+        {
+            throw new ArgumentException("Unexpected value type.");
+        }
+
+        return string.Join(", ", courses.Select(x => x.Name));
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
