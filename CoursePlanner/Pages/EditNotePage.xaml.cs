14d68a68ad060a98c6b2e33a83a406859f3b705e
* <tbui17@users.noreply.github.com>
2024-08-17 23:21:02 -0600

    [course;note]{view}(C3) course and edit note page

---
+++ CoursePlanner/Pages/EditNotePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ViewModels.PageViewModels;
+
+namespace CoursePlanner.Pages;
+
+public partial class EditNotePage : ContentPage
+{
+    public EditNotePage(EditNoteViewModel model)
+    {
+        Model = model;
+        InitializeComponent();
+        HideSoftInputOnTapped = true;
+        BindingContext = Model;
+    }
+
+    public EditNoteViewModel Model { get; set; }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Model.RefreshAsync();
+    }
+}
