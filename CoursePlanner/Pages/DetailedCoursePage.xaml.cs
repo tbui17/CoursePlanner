a05d13d747af88fe7af5197818c4c9c783736f5f
* <tbui17@users.noreply.github.com>
2024-08-17 23:12:34 -0600

    [course;note]{view}(C3) Add detailed view for course Add edit views for course, note, instructor

---
+++ CoursePlanner/Pages/DetailedCoursePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ViewModels.PageViewModels;
+
+namespace CoursePlanner.Pages;
+
+public partial class DetailedCoursePage
+{
+    public DetailedCourseViewModel Model { get; }
+
+    public DetailedCoursePage(DetailedCourseViewModel model)
+    {
+        Model = model;
+        InitializeComponent();
+        HideSoftInputOnTapped = true;
+        BindingContext = model;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Model.RefreshAsync();
+    }
+}
