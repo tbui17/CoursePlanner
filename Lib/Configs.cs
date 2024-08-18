983c95997082bc66655f309d302192240f8e3f23
* <tbui17@users.noreply.github.com>
2024-08-17 21:11:21 -0600

    [lib] configs/utils

---
+++ Lib/Configs.cs
@@ -0,0 +1,15 @@
+﻿using Lib.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lib;
+
+public class Configs
+{
+    public static IServiceCollection ConfigBackendServices(IServiceCollection b)
+    {
+        b
+           .AddTransient<NotificationService>()
+           .AddTransient<ICourseService, CourseService>();
+        return b;
+    }
+}
