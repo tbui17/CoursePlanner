7075751e216b9c652b6865dd9ded865bc45218ae
* <tbui17@users.noreply.github.com>
2024-08-17 21:25:56 -0600

    [term]{vm}(B1) Add term CRUD functionality

---
+++ ViewModels/PageViewModels/DetailedTermViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Core.Extensions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lib.Models;
+using Microsoft.EntityFrameworkCore;
+using ViewModels.Services;
+
+namespace ViewModels.PageViewModels;
+
+
+public partial class DetailedTermViewModel(ILocalDbCtxFactory factory, INavigationService navService, IAppService appService)
+    : ObservableObject
+{
+    [ObservableProperty]
+    private int _id;
+
+    [NotifyPropertyChangedFor(nameof(Courses))]
+    [ObservableProperty]
+    private Term _term = new();
+
+    [ObservableProperty]
+    private Course? _selectedCourse;
+
+
+    public ObservableCollection<Course> Courses => Term.Courses.ToObservableCollection();
+
+
+    public async Task Init(int id)
+    {
+        Id = id;
+        await using var db = await factory.CreateDbContextAsync();
+        var res = await db
+               .Terms
+               .Include(x => x.Courses)
+               .ThenInclude(x => x.Instructor)
+               .FirstOrDefaultAsync(x => x.Id == id) ??
+            new();
+        Term = res;
+    }
+
+    public async Task RefreshAsync()
+    {
+        await Init(Id);
+    }
+
+    [RelayCommand]
+    public async Task EditTermAsync()
+    {
+        await navService.GoToEditTermPageAsync(Id);
+    }
+
+
+    [RelayCommand]
+    public async Task BackAsync()
+    {
+        await navService.PopAsync();
+    }
+
+    [RelayCommand]
+    public async Task AddCourseAsync()
+    {
+        await using var db = await factory.CreateDbContextAsync();
+
+        var courseCount = await db.Courses.CountAsync(x => x.TermId == Term.Id);
+        if (courseCount >= 6)
+        {
+            await appService.ShowErrorAsync("You can only have up to 6 courses per term");
+            return;
+        }
+
+        var name = await appService.DisplayNamePromptAsync();
+        if (name is null)
+        {
+            return;
+        }
+
+        var course = Term.CreateCourse();
+        course.Name = name;
+
+        db.Courses.Add(course);
+        await db.SaveChangesAsync();
+        await RefreshAsync();
+    }
+
+    [RelayCommand]
+    public async Task DeleteCourseAsync()
+    {
+        if (SelectedCourse is null) return;
+        await using var db = await factory.CreateDbContextAsync();
+        await db
+           .Courses.Where(x => x.Id == SelectedCourse.Id)
+           .ExecuteDeleteAsync();
+        await RefreshAsync();
+    }
+
+    [RelayCommand]
+    public async Task DetailedCourseAsync()
+    {
+        if (SelectedCourse is null) return;
+        await navService.GoToDetailedCoursesPageAsync(SelectedCourse.Id);
+    }
+}
---
+++ ViewModels/PageViewModels/EditTermViewModel.cs
@@ -0,0 +1,78 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lib.Interfaces;
+using Lib.Traits;
+using Microsoft.EntityFrameworkCore;
+using ViewModels.Services;
+
+namespace ViewModels.PageViewModels;
+
+public partial class EditTermViewModel(
+    ILocalDbCtxFactory factory,
+    INavigationService navService,
+    IAppService appService)
+    : ObservableObject, IDateTimeEntity
+{
+    [ObservableProperty]
+    private int _id;
+
+    [ObservableProperty]
+    private string _name = "";
+
+    [ObservableProperty]
+    private DateTime _start;
+
+    [ObservableProperty]
+    private DateTime _end;
+
+    [RelayCommand]
+    public async Task SaveAsync()
+    {
+
+        if (this.ValidateNameAndDates() is { } exc)
+        {
+            await appService.ShowErrorAsync(exc.Message);
+            return;
+        }
+
+        var db = await factory.CreateDbContextAsync();
+        var term = await db
+           .Terms
+           .AsTracking()
+           .FirstAsync(x => x.Id == Id);
+
+        term.Name = Name;
+        term.Start = Start;
+        term.End = End;
+
+        await db.SaveChangesAsync();
+        await BackAsync();
+    }
+
+    [RelayCommand]
+    public async Task BackAsync()
+    {
+        await navService.PopAsync();
+    }
+
+    public async Task Init(int termId)
+    {
+
+        await using var db = await factory.CreateDbContextAsync();
+
+        var term = await db
+               .Terms
+               .FirstOrDefaultAsync(x => x.Id == termId) ??
+            new();
+
+        Id = term.Id;
+        Name = term.Name;
+        Start = term.Start;
+        End = term.End;
+    }
+
+    public async Task RefreshAsync()
+    {
+        await Init(Id);
+    }
+}
