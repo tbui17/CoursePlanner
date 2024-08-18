fc1124c0c605b1c8a74ea01852b9f1d2158f839d
* <tbui17@users.noreply.github.com>
2024-08-17 19:55:35 -0600

    [maui] navigation

---
+++ CoursePlanner/Services/AppService.cs
@@ -0,0 +1,141 @@
+﻿using CoursePlanner.Pages;
+using Microsoft.Extensions.Logging;
+using ViewModels.Services;
+
+namespace CoursePlanner.Services;
+
+public class AppService : IAppService, INavigationService
+{
+    private readonly IServiceProvider _provider;
+    private readonly ILocalNotificationService _localNotificationService;
+
+
+    // ReSharper disable once NotAccessedField.Local
+    private readonly ILogger<AppService> _logger;
+
+    public AppService(
+        IServiceProvider provider,
+        ILocalNotificationService localNotificationService,
+        ILogger<AppService> logger
+    )
+    {
+        _provider = provider;
+        _localNotificationService = localNotificationService;
+        _logger = logger;
+        _localNotificationService.StartListening();
+    }
+
+
+
+
+    public async Task ShareAsync(ShareTextRequest request)
+    {
+        await Share.RequestAsync(request);
+    }
+
+
+    private T Resolve<T>() where T : notnull => _provider.GetRequiredService<T>();
+
+    private static Shell Current => Shell.Current;
+
+    private static INavigation Navigation => Current.Navigation;
+
+    public async Task ShowErrorAsync(string message)
+    {
+        await Current.DisplayAlert("Error", message, "OK");
+    }
+
+
+    private async Task GoToAsync<T>(Func<T, int> setter) where T : Page
+    {
+        var page = Resolve<T>();
+        setter(page);
+        await Navigation.PushAsync(page);
+    }
+
+
+    public async Task GoToMainPageAsync()
+    {
+        await Current.GoToAsync($"///{nameof(MainPage)}");
+    }
+
+    private static async Task GoToAsync<T>(IDictionary<string, object>? query = null) where T : ContentPage
+    {
+        var route = $"{typeof(T).Name}";
+        if (query is not null)
+        {
+            await Current.GoToAsync(route, query);
+        }
+        else
+        {
+            await Current.GoToAsync(route);
+        }
+    }
+
+    public async Task GoToDetailedTermPageAsync(int termId)
+    {
+        await GoToAsync<DetailedTermPage>(x => x.Model.Id = termId);
+    }
+
+    public async Task GoBackToDetailedTermPageAsync()
+    {
+        await Navigation.PopAsync();
+    }
+
+    public async Task<string?> DisplayNamePromptAsync()
+    {
+        return await Current.CurrentPage.DisplayPromptAsync("Enter name", "");
+    }
+
+    public async Task GoToDetailedCoursesPageAsync(int id)
+    {
+        await GoToAsync<DetailedCoursePage>(x => x.Model.Id = id);
+    }
+
+    public async Task GoToEditTermPageAsync(int id)
+    {
+        await GoToAsync<EditTermPage>(x => x.Model.Id = id);
+    }
+
+    public async Task GoToEditCoursePageAsync(int courseId)
+    {
+        await GoToAsync<EditCoursePage>(x => x.Model.Id = courseId);
+    }
+
+    public async Task GoToAddInstructorPageAsync()
+    {
+        var page = Resolve<InstructorFormPage>();
+        page.Model.SetAdding();
+
+        await Navigation.PushAsync(page);
+    }
+
+    public async Task GotoEditInstructorPageAsync(int instructorId)
+    {
+        var page = Resolve<InstructorFormPage>();
+
+        page.Model.SetEditing(instructorId);
+
+        await Navigation.PushAsync(page);
+    }
+
+    public async Task GoToAssessmentDetailsPageAsync(int assessmentId)
+    {
+        await GoToAsync<EditAssessmentPage>(x => x.Model.Id = assessmentId);
+    }
+
+    public async Task GoToNoteDetailsPageAsync(int noteId)
+    {
+        await GoToAsync<EditNotePage>(x => x.Model.Id = noteId);
+    }
+
+    public async Task PopAsync()
+    {
+        await Navigation.PopAsync();
+    }
+
+    public async Task AlertAsync(string message)
+    {
+        await Current.DisplayAlert("Alert", message, "OK");
+    }
+}
