c7ec59967205378aa1cb66572d59b8a73a5d0e31
* <tbui17@users.noreply.github.com>
2024-08-17 21:11:59 -0600

    [viewmodels] interfaces

---
+++ ViewModels/Services/INavigationService.cs
@@ -0,0 +1,16 @@
+﻿namespace ViewModels.Services;
+
+public interface INavigationService
+{
+    Task GoToMainPageAsync();
+    Task GoToDetailedTermPageAsync(int termId);
+    Task GoToDetailedCoursesPageAsync(int id);
+    Task GoToEditTermPageAsync(int id);
+    Task GoToEditCoursePageAsync(int courseId);
+    Task GoToAddInstructorPageAsync();
+    Task GotoEditInstructorPageAsync(int instructorId);
+    Task GoToAssessmentDetailsPageAsync(int assessmentId);
+    Task GoToNoteDetailsPageAsync(int noteId);
+    Task GoBackToDetailedTermPageAsync();
+    Task PopAsync();
+}
