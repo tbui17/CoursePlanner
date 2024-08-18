041afe7f51cb0e1956d6dfce27707d17feddcafb
* <tbui17@users.noreply.github.com>
2024-08-17 21:08:54 -0600

    [lib] traits

---
+++ Lib/Traits/ValidationTrait.cs
@@ -0,0 +1,28 @@
+﻿using Lib.Exceptions;
+using Lib.Interfaces;
+
+namespace Lib.Traits;
+
+public static class ValidationTrait
+{
+
+    public static DomainException? ValidateName<T>(this T entity) where T : IEntity
+    {
+        return string.IsNullOrWhiteSpace(entity.Name)
+            ? new DomainException("Name cannot be null or empty")
+            : null;
+    }
+
+    private static DomainException? ValidateDates<T>(this T dateTimeRange) where T : IDateTimeRange
+    {
+        return dateTimeRange.Start >= dateTimeRange.End
+            ? new DomainException("Start must be before end")
+            : null;
+    }
+
+    public static DomainException? ValidateNameAndDates<T>(this T entity) where T : IEntity, IDateTimeRange
+    {
+        return AggregateValidation(entity.ValidateName(), entity.ValidateDates());
+    }
+
+}
