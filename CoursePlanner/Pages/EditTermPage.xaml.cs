7139e479b0c7b0004da496689fee6876e3dfe6bd
* <tbui17@users.noreply.github.com>
2024-08-17 23:21:29 -0600

    [term]{view}(C1) Term view & main page

---
+++ CoursePlanner/Pages/EditTermPage.xaml.cs
@@ -0,0 +1,24 @@
+
+
+using ViewModels.PageViewModels;
+
+namespace CoursePlanner.Pages;
+
+public partial class EditTermPage
+{
+    public EditTermViewModel Model { get; set; }
+
+    public EditTermPage(EditTermViewModel model)
+    {
+        Model = model;
+        InitializeComponent();
+        HideSoftInputOnTapped = true;
+        BindingContext = Model;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Model.RefreshAsync();
+    }
+}
