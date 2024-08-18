2baac0ae2e99337df30cb990f1c43ecbc13f824e
* <tbui17@users.noreply.github.com>
2024-08-17 21:09:06 -0600

    [lib] utils

---
+++ Lib/Utils/DbUtil.cs
@@ -0,0 +1,21 @@
+﻿using Lib.Models;
+
+namespace Lib.Utils;
+
+public class DbUtil(LocalDbCtx db)
+{
+
+
+    public async Task SeedDatabase()
+    {
+        var dataFactory = new TestDataFactory();
+        await dataFactory.SeedDatabase(db);
+    }
+
+    public async Task ResetAndSeedDb()
+    {
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+        await SeedDatabase();
+    }
+}
