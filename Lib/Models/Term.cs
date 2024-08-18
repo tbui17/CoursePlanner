2d38f92a58c8fbdac9a7906a44957e9411ccdf65
* <tbui17@users.noreply.github.com>
2024-08-17 21:08:28 -0600

    [lib] models

---
+++ Lib/Models/Term.cs
@@ -0,0 +1,24 @@
+﻿using Lib.Interfaces;
+
+namespace Lib.Models;
+
+public class Term : IEntity, IDateTimeRange
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime Start { get; set; } = DefaultStart();
+    public DateTime End { get; set; } = DefaultEnd();
+
+    public ICollection<Course> Courses { get; set; } = [];
+
+
+    public Course CreateCourse()
+    {
+        return new Course
+        {
+            TermId = Id, Start = Start, End = End,
+        };
+    }
+
+
+}
