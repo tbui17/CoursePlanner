7171429773b54d3dc30736f7315fc0584e63080d
* <tbui17@users.noreply.github.com>
2024-08-17 21:43:01 -0600

    [term]{view}(C2) Add detailed term view, constraint 6 courses max

---
+++ CoursePlanner/Pages/DetailedTermPage.xaml.cs
@@ -0,0 +1,23 @@
+using ViewModels.PageViewModels;
+
+namespace CoursePlanner.Pages;
+
+public partial class DetailedTermPage
+{
+    public DetailedTermViewModel Model { get; set; }
+
+    public DetailedTermPage(DetailedTermViewModel model)
+    {
+        Model = model;
+        InitializeComponent();
+        HideSoftInputOnTapped = true;
+        BindingContext = model;
+    }
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Model.RefreshAsync();
+    }
+}
