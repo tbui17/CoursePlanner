d9e1bc09b8851b173ec1667f258da63c4f8355ab
* <tbui17@users.noreply.github.com>
2024-08-17 21:19:42 -0600

    [assessment]{view}(C5) Add edit assessment page view

---
+++ CoursePlanner/Pages/EditAssessmentPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ViewModels.PageViewModels;
+
+namespace CoursePlanner.Pages;
+
+public partial class EditAssessmentPage : ContentPage
+{
+    public EditAssessmentPage(EditAssessmentViewModel model)
+    {
+        Model = model;
+        InitializeComponent();
+        HideSoftInputOnTapped = true;
+        BindingContext = Model;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Model.RefreshAsync();
+    }
+
+    public EditAssessmentViewModel Model { get; set; }
+}
