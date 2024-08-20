f508da42afa2bd53643980a623f3a9262ce61e48
* <tbui17@users.noreply.github.com>
2024-08-20 14:03:08 -0600

    vm

---
+++ ViewModels/PageViewModels/LoginViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lib.Models;
+using Lib.Services;
+using Lib.Utils;
+using Microsoft.Extensions.Logging;
+using ViewModels.Services;
+
+
+namespace ViewModels.PageViewModels;
+
+public partial class LoginViewModel(
+    AccountService accountService,
+    IAppService appService,
+    INavigationService navService,
+    ILogger<DetailedCourseViewModel> logger) : ObservableObject, ILogin
+{
+    [ObservableProperty]
+    private string _username = "";
+
+    [ObservableProperty]
+    private string _password = "";
+
+
+    [RelayCommand]
+    public async Task LoginAsync()
+    {
+        var res = await accountService.LoginAsync(new LoginDetails(this));
+
+        if (res.IsFailed)
+        {
+            await appService.ShowErrorAsync(res.ToErrorString());
+        }
+
+        await navService.GoToPageAsync(NavigationTarget.MainPage);
+    }
+
+
+    [RelayCommand]
+    public async Task RegisterAsync()
+    {
+        var res = await accountService.CreateAsync(new LoginDetails(this));
+
+        if (res.IsFailed)
+        {
+            await appService.ShowErrorAsync(res.ToErrorString());
+        }
+
+        await navService.GoToPageAsync(NavigationTarget.MainPage);
+    }
+
+}
