15501deb9efb8b0ab57029a214c4a3ea6d827858
* <tbui17@users.noreply.github.com>
2024-08-25 01:03:11 -0600

    Revise tests, fix inconsistent index on grid state, expose additional properties in interface

--- Lib/Utils/GridState.cs
+++ Lib/Utils/GridState.cs
@@ -12,14 +12,14 @@ public abstract record GridState
         init => _columns = Math.Max(value, 1);
     }
 
-    protected int Index => Count - 1;
+    public int Index => Math.Max(0,Count - 1);
 
     public int Row =>
         Count <= Columns
             ? 0
             : Index / Columns;
 
-    protected int Count => ChildCount();
+    public int Count => ChildCount();
 
     public int Column =>
         Columns <= 1 || Count <= 1
@@ -39,6 +39,8 @@ public interface IAutoGridState
     int Rows { get; }
     int Row { get; }
     int Column { get; }
+    int Count { get; }
+    int Index { get; }
     bool ShouldAddRowDefinition { get; }
 }
 
