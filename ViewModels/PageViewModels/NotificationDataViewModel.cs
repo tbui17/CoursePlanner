cdb5f3a22ad1a2b79a6478eb078b365e95b94735
* <tbui17@users.noreply.github.com>
2024-08-28 15:18:32 -0600

    [fix] remove source generator; caused release deployments to break

--- ViewModels/PageViewModels/NotificationDataViewModel.cs
+++ ViewModels/PageViewModels/NotificationDataViewModel.cs
@@ -1,44 +1,58 @@
-using System.Collections.Immutable;
 using System.Reactive.Linq;
 using Lib.Interfaces;
 using Lib.Services;
 using ReactiveUI;
-using ReactiveUI.SourceGenerators;
 
 namespace ViewModels.PageViewModels;
 
 using NotificationCollection = List<INotification>;
 
-public partial class NotificationDataViewModel : ReactiveObject
+public class NotificationDataViewModel : ReactiveObject
 {
-    [Reactive]
     private string _filterText = "";
 
-    [Reactive]
+    public string FilterText
+    {
+        get => _filterText;
+        set => this.RaiseAndSetIfChanged(ref _filterText, value);
+    }
+
+
     private DateTime _monthDate = DateTime.Now.Date;
 
-    [ObservableAsProperty]
-    // ReSharper disable once NotAccessedField.Local
-    private NotificationCollection _notificationItems = [];
+    public DateTime MonthDate
+    {
+        get => _monthDate;
+        set => this.RaiseAndSetIfChanged(ref _monthDate, value);
+    }
+
+    private readonly ObservableAsPropertyHelper<NotificationCollection> _notificationItemsHelper;
+
+    public NotificationCollection NotificationItems => _notificationItemsHelper.Value;
 
     public NotificationDataViewModel(NotificationService service)
     {
         var filterStream = this.WhenAnyValue(vm => vm.FilterText)
-            .Throttle(TimeSpan.FromMilliseconds(500))
-            .Select(text => text.Trim().ToLowerInvariant());
+            .Throttle(TimeSpan.FromMilliseconds(500));
 
         var monthStream = this.WhenAnyValue(vm => vm.MonthDate)
             .SelectMany(service.GetNotificationsForMonth);
 
-        _notificationItemsHelper = monthStream
+        var dataStream = monthStream
+            .ObserveOn(RxApp.TaskpoolScheduler)
             .CombineLatest(filterStream)
             .Select(pair =>
             {
                 var (notifications, filterText) = pair;
-                return notifications.Where(item => item.Name.Contains(filterText));
+                return notifications
+                    .AsParallel()
+                    .Where(item => item.Name.Contains(filterText, StringComparison.CurrentCultureIgnoreCase));
             })
-            .Select(x => x.ToList())
-            .LoggedCatch(this,Observable.Return(new NotificationCollection()))
+            .Select(x => x.ToList());
+
+        _notificationItemsHelper = dataStream
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .LoggedCatch(this, Observable.Return(new NotificationCollection()))
             .ToProperty(this, vm => vm.NotificationItems);
     }
 }
