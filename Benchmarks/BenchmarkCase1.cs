6d9501ddc2e09d3f821e3f6707351edf84721e97
* <tbui17@users.noreply.github.com>
2024-08-31 16:51:49 -0600

    add benchmark

---
+++ Benchmarks/BenchmarkCase1.cs
@@ -0,0 +1,25 @@
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+public class BenchmarkCase1
+{
+    private const int N = 10000;
+    private readonly byte[] data;
+
+    private readonly SHA256 sha256 = SHA256.Create();
+    private readonly MD5 md5 = MD5.Create();
+
+    public BenchmarkCase1()
+    {
+        data = new byte[N];
+        new Random(42).NextBytes(data);
+    }
+
+    [Benchmark]
+    public byte[] Sha256() => sha256.ComputeHash(data);
+
+    [Benchmark]
+    public byte[] Md5() => md5.ComputeHash(data);
+}
