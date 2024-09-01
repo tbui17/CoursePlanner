29a4753094543ea3a782fb079c43433f25dce417
* <tbui17@users.noreply.github.com>
2024-08-31 23:12:58 -0600

    stats view model

--- ViewModels/Domain/StatsViewModel.cs
+++ ViewModels/Domain/StatsViewModel.cs
@@ -1,11 +1,20 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using Lib.Interfaces;
+using Lib.Models;
+using Lib.Services.ReportService;
+using ViewModels.Interfaces;
 
 namespace ViewModels.Domain;
 
-
-public class StatsViewModel : ObservableObject
+public partial class StatsViewModel(ReportService reportService) : ObservableObject, IRefresh
 {
 
+    [ObservableProperty]
+    private IDurationReport _durationReport = new AggregateDurationReport();
 
 
+    public async Task RefreshAsync()
+    {
+        DurationReport = await reportService.GetDurationReport();
+    }
 }
