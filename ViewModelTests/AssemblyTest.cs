a21804d3eb3bb462558271e37ace103c8381aca1
* <tbui17@users.noreply.github.com>
2024-09-03 16:40:57 -0600

    test

---
+++ ViewModelTests/AssemblyTest.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using Lib.Utils;
+using ViewModels.Config;
+using ViewModels.Domain;
+
+namespace ViewModelTests;
+
+public class AssemblyTest
+{
+    private ServiceCollection _services;
+    private ViewModelConfig _clientConfig;
+
+    [SetUp]
+    public void Setup()
+    {
+        _services = new ServiceCollection();
+        var assemblyService = new AssemblyService(AppDomain.CurrentDomain);
+
+        _clientConfig = new ViewModelConfig(assemblyService,_services);
+
+    }
+
+    [Test]
+    public void Config_ShouldRetrieveConcreteClassesFromDomainNamespace()
+    {
+
+        _clientConfig.AddServices();
+
+        _services
+            .Should()
+            .ContainSingle(x => x.ServiceType == typeof(InstructorFormViewModelFactory))
+            .And.ContainSingle(x => x.ServiceType == typeof(EditAssessmentViewModel))
+            .And.NotContain(x => x.ServiceType == typeof(IEditAssessmentViewModel));
+    }
+
+}
