4c20703a054900cdb7beb607bb2e5e2b941c84bf
* <tbui17@users.noreply.github.com>
2024-09-09 14:12:27 -0600

    tests

--- ViewModelTests/Domain/ViewModels/NotificationDataViewModelTest.cs
+++ ViewModelTests/Domain/ViewModels/NotificationDataViewModelTest.cs
@@ -1,8 +1,10 @@
+using System.Reactive.Linq;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Lib.Interfaces;
 using Lib.Models;
 using Microsoft.EntityFrameworkCore;
+using ReactiveUI;
 using ViewModels.Domain;
 using ViewModelTests.TestSetup;
 
@@ -29,7 +31,8 @@ public class NotificationDataViewModelTest : BasePageViewModelTest
     [Test]
     public async Task Properties_Initialize_UpdateWithDbValues()
     {
-        Model.NotificationItems.Should()
+        Model.NotificationItems
+            .Should()
             .NotBeNull()
             .And.ContainItemsAssignableTo<Assessment>();
         await Task.CompletedTask;
@@ -39,11 +42,32 @@ public class NotificationDataViewModelTest : BasePageViewModelTest
     [Test]
     public async Task Properties_UserInput_UpdateWithDbValues()
     {
-
         await Model.RefreshAsync();
         Model.Start = DateTime.Now.AddMinutes(2);
         Model.FilterText = "Course";
         await Task.Delay(1000);
+        using var scope = new AssertionScope();
+        Model.NotificationItems.Should()
+            .NotContainItemsAssignableTo<Assessment>()
+            .And.ContainItemsAssignableTo<Course>();
+        Model.ItemCount.Should().BeGreaterThan(0);
+    }
+
+    [Test]
+    public async Task ChangePage_ChangesData()
+    {
+
+        await Model.RefreshAsync();
+        var data = Model.NotificationItems.ToList()
+            .Should()
+            .NotBeEmpty()
+            .And.Subject;
+
+        var changes = new List<bool>();
+
+        Model.WhenAnyValue(x => x.Busy)
+            .Subscribe(x => changes.Add(x));
+        Model.ChangePageCommand.Execute(2);
         using var scope = new AssertionScope();
         Model.NotificationItems.Should()
             .NotContainItemsAssignableTo<Assessment>()
