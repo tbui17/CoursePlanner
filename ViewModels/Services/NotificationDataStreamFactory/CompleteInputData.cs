99a15faa2eab32e5c17845f025ee0ee2f5279327
* <tbui17@users.noreply.github.com>
2024-09-11 12:07:08 -0600

    refactor

--- ViewModels/Services/NotificationDataStreamFactory/CompleteInputData.cs
+++ ViewModels/Services/NotificationDataStreamFactory/CompleteInputData.cs
@@ -1,39 +1,7 @@
-using System.Reactive.Linq;
 using Lib.Interfaces;
 using ViewModels.Domain;
-using ViewModels.Models;
 
 namespace ViewModels.Services.NotificationDataStreamFactory;
-
-public class CompleteInputModel
-{
-    public CompleteInputData Data { get; init; } = new();
-
-    public int PageIndex => Math.Max(0, Data.CurrentPage - 1);
-    public int PartitionSize => Math.Max(1, Data.PageSize);
-
-    private IReadOnlyList<INotification>? _cache;
-
-    public IReadOnlyList<INotification> GetFilteredData()
-    {
-        if (_cache is not null)
-        {
-            return _cache;
-        }
-        var filter = new NotificationDataFilterFactory { Data = Data }.CreateFilter();
-        _cache =  Data.Notifications.AsParallel().Where(filter).ToList();
-        return _cache;
-    }
-
-    public IReadOnlyList<IReadOnlyList<INotification>> GetPaginatedData() =>
-        GetFilteredData()
-            .Chunk(PartitionSize)
-            .ToList();
-
-
-    public IReadOnlyList<INotification> GetCurrentPage() => GetPaginatedData()
-        .ElementAtOrDefault(PageIndex) ?? [];
-}
 
 public record CompleteInputData : IFilterData
 {
@@ -53,18 +21,5 @@ public record CompleteInputData : IFilterData
         notificationSelectedIndex = NotificationSelectedIndex;
         currentPage = CurrentPage;
         pageSize = PageSize;
-    }
-}
-
-public static class CompleteInputModelExtensions
-{
-    public static PageDataStream CreatePageDataStream(this IObservable<CompleteInputModel> stream)
-    {
-        return new PageDataStream
-        {
-            Data = stream.Select(x => x.GetCurrentPage().ToList()),
-            PageCount = stream.Select(x => x.GetPaginatedData().Count),
-            ItemCount = stream.Select(x => x.GetCurrentPage().Count)
-        };
     }
 }
