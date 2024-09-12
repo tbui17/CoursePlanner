c6b2bda5e510e0a55f425dd55cbcf618c5d6c230
* <tbui17@users.noreply.github.com>
2024-09-12 15:05:28 -0600

    inject

--- ViewModels/Domain/NotificationDataViewModel/DefaultPageProvider.cs
+++ ViewModels/Domain/NotificationDataViewModel/DefaultPageProvider.cs
@@ -1,5 +1,8 @@
+using Lib.Attributes;
+
 namespace ViewModels.Domain.NotificationDataViewModel;
 
+[Inject(typeof(IDefaultPageProvider))]
 public sealed class DefaultPageProvider : IDefaultPageProvider
 {
     public int PageSize => 10;
