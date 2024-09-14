198b2a06a8c7f93d782234054a7d1200f7e9cb73
* <tbui17@users.noreply.github.com>
2024-09-14 08:20:54 -0600

    add cache logging

--- Lib/Services/NotificationService/NotificationTypeProvider.cs
+++ Lib/Services/NotificationService/NotificationTypeProvider.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using Humanizer;
 using Lib.Attributes;
 using Lib.Interfaces;
@@ -6,39 +5,58 @@ using Lib.Models;
 using Lib.Utils;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Lib.Services.NotificationService;
 
-[SuppressMessage("ReSharper", "RedundantNameQualifier")]
 [Inject(Lifetime = ServiceLifetime.Singleton)]
-public class NotificationTypeProvider(IMemoryCache cache)
+public class NotificationTypeProvider(IMemoryCache cache, ILogger<NotificationTypeProvider> logger)
 {
     public List<string> GetNotificationTypes()
     {
-
-        if (!cache.TryGetValue<List<string>>(nameof(GetNotificationTypes), out var types))
+        if (cache.TryGetValue<List<string>>(nameof(GetNotificationTypes), out var types))
         {
-
-            types = GetTypes();
-            cache.Set(nameof(GetNotificationTypes), types);
-            return types;
-
+            return types!;
         }
 
+        types = GetTypes();
+        cache.Set(nameof(GetNotificationTypes),
+            types,
+            new MemoryCacheEntryOptions
+            {
+                PostEvictionCallbacks =
+                {
+                    new PostEvictionCallbackRegistration { EvictionCallback = EvictionCallback }
+                }
+            }
+        );
 
-        return types!;
+        return types;
 
-        List<string> GetTypes()
+        void EvictionCallback(
+            object key,
+            object? value,
+            EvictionReason reason,
+            object? state
+        )
         {
-            var assessmentTypes = Assessment.Types.Select(type => $"{type} {nameof(Assessment)}");
-            return DbContextUtil.GetEntityTypes<LocalDbCtx, INotification>()
-                .Where(x => x != typeof(Assessment))
-                .Select(x => x.Name)
-                .Concat(assessmentTypes)
-                .Select(x => x.Humanize(LetterCasing.Title))
-                .ToList();
+            logger.LogInformation("Cache entry {Key} with value {Value} was evicted due to {Reason}. {State}",
+                key,
+                value,
+                reason,
+                state
+            );
         }
     }
 
-
+    private static List<string> GetTypes()
+    {
+        var assessmentTypes = Assessment.Types.Select(type => $"{type} {nameof(Assessment)}");
+        return DbContextUtil.GetEntityTypes<LocalDbCtx, INotification>()
+            .Where(x => x != typeof(Assessment))
+            .Select(x => x.Name)
+            .Concat(assessmentTypes)
+            .Select(x => x.Humanize(LetterCasing.Title))
+            .ToList();
+    }
 }
