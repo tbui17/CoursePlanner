1477d05d05a6d91c9c43d1bc1fa9e775c67d3346
* <tbui17@users.noreply.github.com>
2024-09-14 00:25:05 -0600

    Config DI

--- Lib/Config/BackendConfig.cs
+++ Lib/Config/BackendConfig.cs
@@ -3,14 +3,16 @@ using Lib.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 
-
 namespace Lib.Config;
 
 public static class BackendConfig
 {
     public static IServiceCollection AddBackendServices(this IServiceCollection services)
     {
-        services.AddValidatorsFromAssemblyContaining<LoginFieldValidator>(ServiceLifetime.Transient);
+        services.AddValidatorsFromAssemblyContaining<LoginFieldValidator>(ServiceLifetime.Transient)
+            .AddTransient<AppDomain>(_ => AppDomain.CurrentDomain)
+            .AddMemoryCache();
+
         return services;
     }
 }
