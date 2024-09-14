f54ceec886c35315690963eb54014255d3f5ebd4
* <tbui17@users.noreply.github.com>
2024-09-13 21:06:05 -0600

    Add refresh test

---
+++ ViewModelTests/Domain/ViewModels/NotificationDataViewModelTest/RefreshTest.cs
@@ -0,0 +1,27 @@
+using FluentAssertions;
+using ViewModelTests.TestSetup;
+using ViewModelTests.Utils;
+
+namespace ViewModelTests.Domain.ViewModels.NotificationDataViewModelTest;
+
+public class RefreshTest : BasePageViewModelTest
+{
+    [Test]
+    public async Task Refresh_ContinuesToWorkAfterFirstUse()
+    {
+        var f = Resolve<NotificationDataPaginationTestFixture>();
+        f.DataService.Invocations.Clear();
+        const int count = 10;
+
+        for (var i = 0; i < count; i++)
+        {
+            await f.Model.RefreshAsync();
+        }
+
+        await f.DataService.Invocations.ShouldEventuallySatisfy(x => x
+            .Should()
+            .HaveCount(count)
+        );
+
+    }
+}
