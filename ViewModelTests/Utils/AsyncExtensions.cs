9bdadabbc66d0bab28beec72432fa882ddfbc61a
* <tbui17@users.noreply.github.com>
2024-09-13 21:06:05 -0600

    Add refresh test

--- ViewModelTests/Utils/AsyncExtensions.cs
+++ ViewModelTests/Utils/AsyncExtensions.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Text;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
 namespace ViewModelTests.Utils;
 
 public static class AsyncExtensions
@@ -11,5 +16,51 @@ public static class AsyncExtensions
         }
 
         return predicate(obj);
+    }
+
+    public static async Task<AndConstraint<T>> ShouldEventuallySatisfy<T>(this T subject, Action<T> assertion, int timeoutMs = 5000)
+    {
+        var scope = new AssertionScope();
+        var cts = new CancellationTokenSource(timeoutMs);
+
+        string[] errors = [];
+
+        while (!cts.Token.IsCancellationRequested)
+        {
+
+            var scope2 = new AssertionScope();
+            try
+            {
+                assertion(subject);
+            }
+            catch (NullReferenceException)
+            {
+                scope2.AddReportable("subject",() => subject?.ToString());
+                var sb = new StringBuilder();
+
+                sb
+                    .AppendLine("Encountered null reference exception during assertion.")
+                    .AppendLine("Subject: {subject}");
+
+                scope2.FailWith(sb.ToString());
+            }
+            if (!scope2.HasFailures())
+            {
+                return new AndConstraint<T>(subject);
+            }
+
+            errors = scope2.Discard();
+            await Task.Delay(100);
+        }
+
+
+        if (errors.Length > 0)
+        {
+            scope.FailWith(string.Join("\n", errors));
+        }
+
+        scope.FailWith("Expected assertion to pass within {0}ms, but it did not.", timeoutMs);
+        scope.Dispose();
+        throw new UnreachableException();
     }
 }
