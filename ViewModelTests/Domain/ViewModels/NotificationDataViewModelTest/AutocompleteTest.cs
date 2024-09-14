db0d7a8582a7de2bb9356ca237d454a0eeaca220
* <tbui17@users.noreply.github.com>
2024-09-14 00:26:16 -0600

    Refactor test, add check for unrelated types

--- ViewModelTests/Domain/ViewModels/NotificationDataViewModelTest/AutocompleteTest.cs
+++ ViewModelTests/Domain/ViewModels/NotificationDataViewModelTest/AutocompleteTest.cs
@@ -1,4 +1,7 @@
 using FluentAssertions;
+using Lib.Interfaces;
+using Lib.Models;
+using Lib.Utils;
 using ViewModels.Domain.NotificationDataViewModel;
 using ViewModelTests.TestSetup;
 
@@ -7,14 +10,35 @@ namespace ViewModelTests.Domain.ViewModels.NotificationDataViewModelTest;
 public class AutocompleteTest : BaseTest
 {
     [Test]
-    public void Types_ContainsNotificationItems()
+    public void Types_ContainsCourseNotificationItem()
     {
         var model = Resolve<NotificationDataViewModel>();
+
+
+        model.Types
+            .Should()
+            .NotBeEmpty()
+            .And.Contain(nameof(Course));
+    }
+
+    private static List<string> GetEntities()
+    {
+        return DbContextUtil.GetEntityTypes<LocalDbCtx, INotification>().Select(x => x.Name).ToList();
+    }
+
+    [Test]
+    public void Types_OnlyContainsEntriesRelatedToDomainModels()
+    {
+        var model = Resolve<NotificationDataViewModel>();
+
+        var entityNames = GetEntities();
 
         model.Types
             .Should()
             .HaveCountGreaterThan(1)
-            .And.Contain("Course")
-            .And.Contain(x => x.Contains("Assessment"));
+            .And.AllSatisfy(typeText => entityNames
+                .Should()
+                .Contain(entityName => typeText.Contains(entityName, StringComparison.OrdinalIgnoreCase))
+            );
     }
 }
