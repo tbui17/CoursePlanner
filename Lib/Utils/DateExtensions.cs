fb3fe508c6c695025b583ea122e4696d05a237f0
* <tbui17@users.noreply.github.com>
2024-09-18 17:22:24 -0600

    date utils

--- Lib/Utils/DateExtensions.cs
+++ Lib/Utils/DateExtensions.cs
@@ -5,4 +5,8 @@ public static class DateExtensions
     public static TimeSpan Min(this TimeSpan target, TimeSpan max) => target > max ? max : target;
     public static TimeSpan Max(this TimeSpan target, TimeSpan min) => target < min ? min : target;
     public static TimeSpan Clamp(this TimeSpan target, TimeSpan min, TimeSpan max) => target.Max(min).Min(max);
+
+    public static DateTime Min(this DateTime target, DateTime max) => target > max ? max : target;
+    public static DateTime Max(this DateTime target, DateTime min) => target < min ? min : target;
+    public static DateTime Clamp(this DateTime target, DateTime min, DateTime max) => target.Max(min).Min(max);
 }
