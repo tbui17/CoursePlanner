b3b038d65316b03f56e0f5a12c87a49ad3645d42
* <tbui17@users.noreply.github.com>
2024-09-18 17:26:56 -0600

    fix incorrect calculations includes but not limited to: completed time, remaining time

--- Lib/Services/ReportService/ReportService.cs
+++ Lib/Services/ReportService/ReportService.cs
@@ -9,22 +9,24 @@ namespace Lib.Services.ReportService;
 [Inject]
 public class ReportService(MultiLocalDbContextFactory dbFactory)
 {
-    public async Task<IDurationReport> GetDurationReport()
+    public async Task<IDurationReport> GetAggregateDurationReportData(DateTime? date = null)
     {
-        return new AggregateDurationReportFactory { Reports = await GetReports() }.Create();
+        var time = date ?? DateTime.Now.Date;
+        return new AggregateDurationReportFactory { Reports = await GetDurationReportData(time)}.Create();
+    }
 
-        async Task<IList<DurationReport>> GetReports()
-        {
-            var now = DateTime.Now.Date;
-            await using var db = await dbFactory.CreateAsync<IDateTimeEntity>();
-            return await db.Query(q => Task.Run(async () =>
+    public async Task<IReadOnlyCollection<DurationReportFactory>> GetDurationReportData(DateTime? date = default)
+    {
+        var now = date ?? DateTime.Now.Date;
+        await using var db = await dbFactory.CreateAsync<IDateTimeRange>();
+
+        var res = await db.QueryThreaded(async q =>
             {
-                var res = await q.AsNoTracking().ToListAsync();
-                var fac = new DurationReportFactory { Entities = res, Date = now };
-                var results = fac.Create();
-                res.Clear();
-                return results;
-            }));
-        }
+                var res = await q.AsNoTracking().Select(x => new DateTimeRange{Start = x.Start, End = x.End}).ToListAsync();
+                var fac = new DurationReportFactory { Entities = res, Date = now,Type = q.ElementType};
+                return fac;
+            }
+        );
+        return res.AsReadOnly();
     }
 }
