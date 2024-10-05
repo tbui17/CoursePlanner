1e41ef973adc0c4166031dedb24c5266c92f636f
* <tbui17@users.noreply.github.com>
2024-10-05 16:08:11 -0600

    c

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -47,8 +47,7 @@ resource "google_iam_workload_identity_pool_provider" "main" {
     (local.attribute_repository_const) = "assertion.repository"
   }
   oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com",
-    allowed_audiences = []
+    issuer_uri = "https://token.actions.githubusercontent.com"
   }
 }
 
