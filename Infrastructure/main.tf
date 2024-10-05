1cb14e5f764d1f973bed342fe5442129b28c751f
* <tbui17@users.noreply.github.com>
2024-10-05 16:48:07 -0600

    c

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -42,10 +42,10 @@ resource "google_iam_workload_identity_pool_provider" "main" {
   display_name                       = "${local.github_actions_display_name_const} Provider"
   attribute_condition                = "assertion.repository_owner=='${var.repository_owner}'"
   attribute_mapping = {
-    google.subject             = "assertion.sub"
-    attribute.actor            = "assertion.actor"
+    "google.subject"             = "assertion.sub"
+    "attribute.actor"            = "assertion.actor"
     (local.attribute_repository_const) = "assertion.repository"
-    attribute.repository_owner = "assertion.repository_owner"
+    "attribute.repository_owner" = "assertion.repository_owner"
   }
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
@@ -62,8 +62,7 @@ resource "github_actions_secret" "action_secret" {
     "GOOGLE_SERVICE_ACCOUNT" : jsonencode(var.google_service_account),
     "KEYSTORE_CONTENTS" : var.keystore_contents,
     "GOOGLE_SERVICE_ACCOUNT_BASE64" : var.google_service_account_base64,
-    "KEY_URI" : var.key_uri,
-    "WORKLOAD_IDENTITY_PROVIDER" : local.workload_identity_provider,
+    "KEY_URI" : var.key_uri
   })
   repository      = var.repository_name
   secret_name     = each.key
@@ -80,11 +79,25 @@ resource "github_actions_variable" "action_var" {
     "ANDROID_SIGNING_KEY_ALIAS" : var.android_signing_key_alias,
     "ANDROID_FRAMEWORK" : var.android_framework,
     "USER_IDENTIFIER" : var.user_identifier,
+    "WORKLOAD_IDENTITY_PROVIDER" : local.workload_identity_provider,
   })
   repository    = var.repository_name
   variable_name = each.key
   value         = each.value
 }
+#
+# module "gh_oidc" {
+#   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+#   project_id  = var.project_id
+#   pool_id     = "${local.github_actions_const}-pool2"
+#   provider_id = "${local.github_actions_const}-provider"
+#   sa_mapping = {
+#     (google_service_account.github_actions.account_id) = {
+#       sa_name   = "projects/courseplanner-437101/serviceAccounts/github-actions-account@courseplanner-437101.iam.gserviceaccount.com"
+#       attribute = "attribute.repository/tbui17/CoursePlanner"
+#     }
+#   }
+# }
 
 locals {
   attribute_repository_const        = "attribute.repository"
