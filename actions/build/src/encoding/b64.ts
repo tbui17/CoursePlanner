2ab405853c98f9ae19120ab4b99de448138f2385
* <tbui17@users.noreply.github.com>
2024-11-17 20:38:49 -0700

    remove unused

--- actions/build/src/encoding/b64.ts
+++
@@ -1,9 +0,0 @@
-import { decorate, withTryCatch } from '../util/decorate'
-
-export function encode(data: Parameters<typeof Buffer.from>[0]): string {
-  return Buffer.from(data).toString('base64')
-}
-
-export function decode(data: string): string {
-  return Buffer.from(data, 'base64').toString('utf-8')
-}
