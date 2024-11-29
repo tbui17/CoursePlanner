b2987fa37ab35e664a7a16d8e785ff93ab041c06
* <tbui17@users.noreply.github.com>
2024-11-29 14:02:05 -0700

    assign remove return type

--- Lib/Traits/AssignmentTrait.cs
+++ Lib/Traits/AssignmentTrait.cs
@@ -4,7 +4,7 @@ namespace Lib.Traits;
 
 public static class AssignmentTrait
 {
-    public static IAssessmentForm Assign(this IAssessmentForm self, IAssessmentForm other)
+    public static void Assign(this IAssessmentForm self, IAssessmentForm other)
     {
         self.Id = other.Id;
         self.Name = other.Name;
@@ -13,11 +13,9 @@ public static class AssignmentTrait
         self.ShouldNotify = other.ShouldNotify;
         self.Type = other.Type;
         self.CourseId = other.CourseId;
-
-        return self;
     }
 
-    public static ICourseField Assign(this ICourseField self, ICourseField other)
+    public static void Assign(this ICourseField self, ICourseField other)
     {
         self.Status = other.Status;
         self.Start = other.Start;
@@ -25,50 +23,38 @@ public static class AssignmentTrait
         self.Id = other.Id;
         self.Name = other.Name;
         self.ShouldNotify = other.ShouldNotify;
-
-        return self;
     }
 
-    public static IContact Assign(this IContact self, IContact other)
+    public static void Assign(this IContact self, IContact other)
     {
         self.Id = other.Id;
         Assign((IContactForm)self, other);
-
-        return self;
     }
 
-    public static IContactForm Assign(this IContactForm self, IContactForm other)
+    public static void Assign(this IContactForm self, IContactForm other)
     {
         self.Name = other.Name;
         self.Email = other.Email;
         self.Phone = other.Phone;
-
-        return self;
     }
 
-    public static INoteField Assign(this INoteField self, INoteField other)
+    public static void Assign(this INoteField self, INoteField other)
     {
         self.Id = other.Id;
         self.Name = other.Name;
         self.Value = other.Value;
-
-        return self;
     }
 
-    public static IDateTimeEntity Assign(this IDateTimeEntity self, IDateTimeEntity other)
+    public static void Assign(this IDateTimeEntity self, IDateTimeEntity other)
     {
         self.Id = other.Id;
         self.Name = other.Name;
         self.Start = other.Start;
         self.End = other.End;
-
-        return self;
     }
 
-    public static IUserSetting Assign(this IUserSetting self, IUserSetting other)
+    public static void Assign(this IUserSetting self, IUserSetting other)
     {
         self.NotificationRange = other.NotificationRange;
-
-        return self;
     }
 }
