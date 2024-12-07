347c2a8d4c80fcfa9f80b0832d829ecc35e87551
* <tbui17@users.noreply.github.com>
2024-12-07 15:29:38 -0700

    comment

--- Lib/Utils/GridState.cs
+++ Lib/Utils/GridState.cs
@@ -7,27 +7,35 @@ public abstract class GridState(Func<int> childCount)
     public int Columns
     {
         get => _columns;
-        init => _columns = Math.Max(value, 1);
+        init => _columns = Math.Max(value, 1); // avoid defining 0 column behavior
     }
 
     public int Count => childCount();
 
-    public int Index => Math.Max(0, Count - 1);
+    public int Index => Math.Max(0, Count - 1); // prevent index errors in unforeseen scenarios
 
     public int Row =>
-        Count <= Columns
+        Count <= Columns // prevent division by 0 and other reasons, don't remember
             ? 0
-            : Index / Columns;
+            : Index / Columns; // repeating sequence
 
     public int Column =>
-        Columns <= 1 || Count <= 1
+        Columns <= 1 ||
+        Count <= 1 // prevent division by 0; when consumer only wants 1 column max we're always at the first; when there's <=1 element we're always at the first
             ? 0
-            : Index % Columns;
+            : Index % Columns; // repeating sequence
 
     public int Rows =>
-        Count <= 0
+        Count <= 0 // if we have no elements in the grid there is no structure of any kind
             ? 0
             : Row + 1;
+}
+
+public class AutoGridState(Func<int> childCount) : GridState(childCount), IAutoGridState
+{
+    public bool ShouldAddRowDefinition =>
+        Column is 0 &&
+        Count > 0; // when we arrive at a new row we're at the first column; we don't add if there is no structure in place (no elements)
 }
 
 public interface IAutoGridState
@@ -38,9 +46,4 @@ public interface IAutoGridState
     int Count { get; }
     int Index { get; }
     bool ShouldAddRowDefinition { get; }
-}
-
-public class AutoGridState(Func<int> childCount) : GridState(childCount), IAutoGridState
-{
-    public bool ShouldAddRowDefinition => Column is 0 && Count > 0;
 }
